e948edb517addcd59b1bffc10aa91ad2df6b3fe2
junho-a1 <kjh974135@gmail.com>
2024-02-06 18:44:53 +0900

    주석 처리 해서 자세히 공부

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require('express'); //app.js는 모든 서버 정보를 갖고 있음
+const helmet = require('helmet');  //require('주소')와 같이 npm 으로 설치한 도구는 바로 도구 이름을 써서 사용할 수 있다.
 const app = express();
 const ejs = require('ejs');
 
@@ -12,9 +12,9 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded());
 
-const mainRouter = require('./router/mainRouter')
+const mainRouter = require('./router/mainRouter')  //mainRouter에 있는 정보를 갖고 옴
 app.use('/',mainRouter)
 
-app.listen(3000,function(req, res){
+app.listen(3000,function(req, res){    
     console.log("서버가 실행됨");
 })
--- router/mainRouter.js
+++ router/mainRouter.js
@@ -1,12 +1,12 @@
-const express = require('express'); //express는 개발을 위한 모든 도구가 있음.
-const router = express.Router();
+const express = require('express'); //express는 개발을 위한 모든 도구를 가져옴.
+const router = express.Router();   //express에서 router라는 도구를 꺼냄
 
 router.get("/", function(req, res){   //request(요구) response(응답) 
-    res.render('index',{title:"EJS 메인페이지"})
+    res.render('index',{title:"EJS 메인페이지"}) //render는 그림파일을 보낼 때 
 })
 
 router.get("/about",function(req, res){
-    res.send('About Page');
+    res.send('About Page'); //문자열이나 숫나 등은 send를 사용 
 })
 
 router.post('/postapi', function(req, res){
@@ -15,4 +15,4 @@ router.post('/postapi', function(req, res){
     res.send('POST API');
 })
 
-module.exports = router;
+module.exports = router; //module.exports를 이용해 mainRouter를 밖으로 내보냄
